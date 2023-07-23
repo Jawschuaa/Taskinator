d51aa5201c275605e350497888598ad52ad5d91d
Joshua Anderson <myasscantpass@gmail.com>
2023-07-23 02:30:23 -0500

    fixed functionality and added features
    fixed add task button and added a clear task button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,23 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var clearTasksButtonEl = document.querySelector("#clear-tasks");
 
-var createTaskHandler = function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
+var createTaskHandler = function () {
+  // Prompt the user for a task description
+  var taskDescription = window.prompt("Enter your task:");
+
+  if (taskDescription) {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = taskDescription;
+    tasksToDoEl.appendChild(listItemEl);
+  }
+};
+
+var clearTasksHandler = function () {
+  // Remove all the child elements (tasks) from the "Tasks To Do" list
+  tasksToDoEl.innerHTML = "";
 };
 
 buttonEl.addEventListener("click", createTaskHandler);
+clearTasksButtonEl.addEventListener("click", clearTasksHandler);
